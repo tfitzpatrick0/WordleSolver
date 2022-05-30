50b3dd48b7bc4b8521c115a682e2889346fb1b75
tfitzpatrick0 <tim.fitz830@gmail.com>
2022-05-29 23:39:02 -0700

    send guess

--- connect.py
+++ connect.py
@@ -1,5 +1,6 @@
 import time
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 # open the website https://www.nytimes.com/games/wordle/index.html
 # find the text input box
@@ -10,10 +11,13 @@ from selenium import webdriver
 def connect():
     driver = webdriver.Chrome("/usr/local/bin/chromedriver")
     driver.get("https://www.nytimes.com/games/wordle/index.html")
-    input_box = driver.find_element_by_id("word")
-    input_box.send_keys("hello")
-    submit_button = driver.find_element_by_id("submit")
-    submit_button.click()
+
+    game = driver.find_element_by_tag_name("body")
+    game.click()
+    time.sleep(1)
+    game.send_keys("crate")
+    game.send_keys(Keys.ENTER)
+    time.sleep(5)
 
 
 def main():
